47a4f2092ea694fc73c7728338a45467ffc260a0
Andrew Lechowicz <andrew@alecho.com>
2023-06-11 22:22:28 -0400

    Maybe fix error?

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -8,7 +8,7 @@ function M.setup()
   local conf = {
     display = {
       open_fn = function()
-        return require("packer.util").float { border = "rounded" }
+        return require("packer.util").float({ border = "rounded" })
       end,
     },
   }
