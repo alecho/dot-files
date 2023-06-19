2ff51a846e887ca4507d32d3a12f913b86d5a29f
Andrew Lechowicz <andrew@alecho.com>
2023-06-19 15:58:01 -0400

    Port some vim configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,23 @@
 require("plugins").setup()
 
-textwidth=80
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "80"
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+vim.opt.autowrite = true
+vim.opt.clipboard = "unnamedplus"
+
+
+vim.cmd([[
+
+" Markdown 80 character limit
+au BufRead,BufNewFile *.md setlocal textwidth=80
+
+" Spell checking
+" Markdown
+autocmd BufRead,BufNewFile *.md setlocal spell
+" Git Commit messages
+autocmd FileType gitcommit setlocal spell
+
+
+]])
