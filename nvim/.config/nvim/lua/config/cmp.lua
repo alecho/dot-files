5a6755d626758ce90c3460a28c7fe2d37e8fa108
Andrew Lechowicz <andrew@alecho.com>
2023-06-19 21:58:48 -0400

    Add LuaJIT 5.2 note

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
   local has_words_before = function()
     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    -- eventually will need to be: local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
   end
