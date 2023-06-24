f5afb5cb5319070c41a0ecfa87117f0c6b2272e3
Andrew Lechowicz <andrew@alecho.com>
2023-06-23 23:44:37 -0400

    Move the formatexpr handler into the documentFormattingProvider check

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -5,12 +5,12 @@ local function on_attach(client, bufnr)
   -- See `:help omnifunc` and `:help ins-completion` for more information.
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Use LSP as the handler for formatexpr.
-  -- See `:help formatexpr` for more information.
-  vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
-
-  -- Format files on write with the LSP
   if client.server_capabilities.documentFormattingProvider then
+    -- Use LSP as the handler for formatexpr.
+    -- See `:help formatexpr` for more information.
+    vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+
+    -- Format files on write with the LSP
     vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format { async = false }]]
   end
 
