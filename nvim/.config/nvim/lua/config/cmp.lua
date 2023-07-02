63a2182f6b900b3fdd2eb7d2966a36875aa9bd87
Andrew Lechowicz <andrew@alecho.com>
2023-07-02 15:21:46 -0400

    Fix inconsistent import

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -8,7 +8,7 @@ function M.setup()
     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
   end
 
-  local luasnip = require "luasnip"
+  local luasnip = require "luaSnip"
   local cmp = require "cmp"
   local lspkind = require "lspkind"
 
