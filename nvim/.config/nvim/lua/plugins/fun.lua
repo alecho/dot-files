bb2b2c37be64d0c5a544717a2cdb344a65a59810
Andrew Lechowicz <andrew@alecho.com>
2023-07-12 20:53:53 -0400

    Wrap cellular-automaton in table

--- nvim/.config/nvim/lua/plugins/fun.lua
+++ nvim/.config/nvim/lua/plugins/fun.lua
@@ -1,11 +1,13 @@
 return {
-  "eandrju/cellular-automaton.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  keys = {
-    { "<leader>1", "<Cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it Rain" },
-    { "<leader>2", "<Cmd>CellularAutomaton game_of_life<CR>", desc = "Conway's Game of Life" },
+  {
+    "eandrju/cellular-automaton.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+      { "<leader>1", "<Cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it Rain" },
+      { "<leader>2", "<Cmd>CellularAutomaton game_of_life<CR>", desc = "Conway's Game of Life" },
+    }
   },
   {
     "folke/drop.nvim",
