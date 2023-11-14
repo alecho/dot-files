9218362b1409ade1d1a532920408cf21c7709fd4
Andrew Lechowicz <andrew@alecho.com>
2023-11-13 21:32:27 -0500

    Add mkdnflow.nvim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -3,5 +3,25 @@ return {
     'ellisonleao/glow.nvim',
     cmd = "Glow",
     opts = {},
+  },
+  {
+    'jakewvincent/mkdnflow.nvim',
+    cmd = "MkdnTableFormat",
+    opts = {
+      modules = {
+        bib = false,
+        buffers = false,
+        conceal = false,
+        cursor = false,
+        folds = false,
+        links = false,
+        lists = false,
+        maps = false,
+        paths = false,
+        tables = true,
+        yaml = false,
+        cmp = false
+      },
+    },
   }
 }
