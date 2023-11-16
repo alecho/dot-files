b72e93a2773ed29707b43241f52bd6e16ce90140
Andrew Lechowicz <andrew@alecho.com>
2023-11-16 11:08:23 -0500

    Add scope colors

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -2,7 +2,21 @@ return {
   -- Indentation
   {
     "lukas-reineke/indent-blankline.nvim",
+    module = "ibl",
     event = "BufRead",
+    config = function()
+      local highlight = {
+        "Operator",    -- fg purple
+        "Keyword",     -- fg cyan
+        "Conditional", -- fg pink
+        "Float",       -- fg orange
+        "Character",   -- fg green
+        "Constant",    -- fg yellow
+        "DiffDelete",  -- fg red
+      }
+
+      require("ibl").setup { scope = { highlight = highlight } }
+    end,
   },
 
   -- Auto pairs
