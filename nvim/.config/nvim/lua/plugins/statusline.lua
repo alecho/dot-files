7bbbe2bcf7d90840c21a1f1b541d4187988c1a9f
Andrew Lechowicz <andrew@alecho.com>
2024-01-10 12:03:21 -0500

    nvim updates

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -3,8 +3,54 @@ return {
     'nvim-lualine/lualine.nvim',
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("config.lualine").setup()
-    end,
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "dracula-nvim",
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1, shorting_target = 150 } },
+        lualine_x = {
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ffb86c" },
+          },
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    }
   }
 }
