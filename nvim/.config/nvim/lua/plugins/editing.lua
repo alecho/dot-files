51ff0e21023f586875192c27e49fd03a18b063b2
Andrew Lechowicz <andrew@alecho.com>
2024-01-19 22:20:29 -0500

    Add text-case plugin

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -71,4 +71,15 @@ return {
     "dkarter/bullets.vim",
     event = "BufRead",
   },
+
+  {
+    'johmsalas/text-case.nvim',
+    event = "BufRead",
+    config = function()
+      require('textcase').setup {}
+      require('telescope').load_extension('textcase')
+      vim.api.nvim_set_keymap('n', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = "Telescope" })
+      vim.api.nvim_set_keymap('v', 'ga.', "<cmd>TextCaseOpenTelescope<CR>", { desc = "Telescope" })
+    end
+  }
 }
