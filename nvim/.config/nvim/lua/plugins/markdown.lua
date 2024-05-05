8d6caa380c9864f3db85da645b0ea22bbe47526d
Andrew Lechowicz <andrew@alecho.com>
2024-05-04 23:10:21 -0400

    Install web based markdown previewer

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -23,5 +23,14 @@ return {
         cmp = false
       },
     },
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
