7a06790643bd5f02cd4c5ca3403b0e4df447785f
Andrew Lechowicz <andrew@alecho.com>
2025-02-08 21:03:08 -0500

    Move autopairs config to plugins dir

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -23,9 +23,23 @@ return {
   {
     "windwp/nvim-autopairs",
     module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
-    event = "VeryLazy",
+    event = "InsertEnter",
     config = function()
-      require("config.autopairs").setup()
+      local np = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
+
+      np.setup({
+        check_ts = true
+      })
+
+      np.add_rules({
+        Rule("*", "*", { "markdown" }):with_pair(cond.not_before_regex("\n")),
+        Rule("_", "_", { "markdown" }):with_pair(cond.before_regex("%s")),
+      })
+      np.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
+      np.add_rules(require('nvim-autopairs.rules.endwise-lua'))
+      np.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
     end,
   },
 
