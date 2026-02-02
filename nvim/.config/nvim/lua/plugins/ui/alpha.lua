d3617fdd8a6a67fa165b16443020de7833da7a67
Andrew Lechowicz <andrew@alecho.com>
2026-02-02 09:15:44 -0500

    nvim startup header from file

--- nvim/.config/nvim/lua/plugins/ui/alpha.lua
+++ nvim/.config/nvim/lua/plugins/ui/alpha.lua
@@ -2,6 +2,33 @@ local function datetime()
   return os.date "\n %d-%m-%Y  %H:%M:%S"
 end
 
+-- Function to load header from file
+local function load_header_from_file(file_path)
+  local file = io.open(file_path, "r")
+  if not file then
+    return nil
+  end
+  
+  local lines = {}
+  for line in file:lines() do
+    table.insert(lines, line)
+  end
+  file:close()
+  
+  return lines
+end
+
+-- Function to get header file path - modify this to change which header is loaded
+local function get_header_file_path()
+  -- Default header file
+  -- return vim.fn.stdpath("config") .. "/headers/al3z.txt"
+  
+  -- Uncomment to use alternative header
+  -- return vim.fn.stdpath("config") .. "/headers/elixir.txt"
+
+  return vim.fn.stdpath("config") .. "/headers/moon_xl.txt"
+end
+
 return {
   'goolord/alpha-nvim',
   event = "VimEnter",
@@ -38,24 +65,32 @@ return {
 
     local dashboard = require "alpha.themes.dashboard"
     local function header()
-      return {
-        [[                    1111111    333333333333333                    ]],
-        [[                   1::::::1   3:::::::::::::::33                  ]],
-        [[                  1:::::::1   3::::::33333::::::3                 ]],
-        [[                  111:::::1   3333333     3:::::3                 ]],
-        [[  aaaaaaaaaaaaa      1::::1               3:::::3zzzzzzzzzzzzzzzzz]],
-        [[  a::::::::::::a     1::::1               3:::::3z:::::::::::::::z]],
-        [[  aaaaaaaaa:::::a    1::::1       33333333:::::3 z::::::::::::::z ]],
-        [[           a::::a    1::::l       3:::::::::::3  zzzzzzzz::::::z  ]],
-        [[    aaaaaaa:::::a    1::::l       33333333:::::3       z::::::z   ]],
-        [[  aa::::::::::::a    1::::l               3:::::3     z::::::z    ]],
-        [[ a::::aaaa::::::a    1::::l               3:::::3    z::::::z     ]],
-        [[a::::a    a:::::a    1::::l               3:::::3   z::::::z      ]],
-        [[a::::a    a:::::a 111::::::1113333333     3:::::3  z::::::zzzzzzzz]],
-        [[a:::::aaaa::::::a 1::::::::::13::::::33333::::::3 z::::::::::::::z]],
-        [[ a::::::::::aa:::a1::::::::::13:::::::::::::::33 z:::::::::::::::z]],
-        [[  aaaaaaaaaa  aaaa111111111111 333333333333333   zzzzzzzzzzzzzzzzz]],
-      }
+      -- Try to load header from file
+      local file_header = load_header_from_file(get_header_file_path())
+      
+      -- Fallback to default header if file loading fails
+      if not file_header then
+        return {
+          [[                    1111111    333333333333333                    ]],
+          [[                   1::::::1   3:::::::::::::::33                  ]],
+          [[                  1:::::::1   3::::::33333::::::3                 ]],
+          [[                  111:::::1   3333333     3:::::3                 ]],
+          [[  aaaaaaaaaaaaa      1::::1               3:::::3zzzzzzzzzzzzzzzzz]],
+          [[  a::::::::::::a     1::::1               3:::::3z:::::::::::::::z]],
+          [[  aaaaaaaaa:::::a    1::::1       33333333:::::3 z::::::::::::::z ]],
+          [[           a::::a    1::::l       3:::::::::::3  zzzzzzzz::::::z  ]],
+          [[    aaaaaaa:::::a    1::::l       33333333:::::3       z::::::z   ]],
+          [[  aa::::::::::::a    1::::l               3:::::3     z::::::z    ]],
+          [[ a::::aaaa::::::a    1::::l               3:::::3    z::::::z     ]],
+          [[a::::a    a:::::a    1::::l               3:::::3   z::::::z      ]],
+          [[a::::a    a:::::a 111::::::1113333333     3:::::3  z::::::zzzzzzzz]],
+          [[a:::::aaaa::::::a 1::::::::::13::::::33333::::::3 z::::::::::::::z]],
+          [[ a::::::::::aa:::a1::::::::::13:::::::::::::::33 z:::::::::::::::z]],
+          [[  aaaaaaaaaa  aaaa111111111111 333333333333333   zzzzzzzzzzzzzzzzz]],
+        }
+      end
+      
+      return file_header
     end
 
     dashboard.section.header.val = header()
